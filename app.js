53855f9e3ffb4a5accf32f6a8129cbe674c824c1
Praval <jindalpraval791@gmail.com>
2022-12-06 14:09:04 +0530

    redeployment

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// extra packages
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -38,7 +39,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
 
 // routes
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,44 +1,44 @@
-const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const { UnauthenticatedError } = require('../errors')
-const jwt = require('jsonwebtoken')
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const { UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-    // const { name, email, password } = req.body;
-    // if (!name || !password || !email) {
-    //     throw new BadRequestError('Please Fill Up All The Details')
-    // }
+  // const { name, email, password } = req.body;
+  // if (!name || !password || !email) {
+  //     throw new BadRequestError('Please Fill Up All The Details')
+  // }
 
-    // const salt = await bcryptJs.genSalt(10);
-    // const hashedPassword = await bcryptJs.hash(password, salt,)
-    // const tempUser = { name, email, password: hashedPassword }
+  // const salt = await bcryptJs.genSalt(10);
+  // const hashedPassword = await bcryptJs.hash(password, salt,)
+  // const tempUser = { name, email, password: hashedPassword }
 
-    const user = await User.create({ ...req.body })
-    // const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', { expiresIn: '30d' })
-    const token = user.createToken();
-    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
-}
+  const user = await User.create({ ...req.body });
+  // const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', { expiresIn: '30d' })
+  const token = user.createToken();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 
 const login = async (req, res) => {
-    // console.log(req.headers);
-    const { email, password } = req.body;
-    if (!email, !password) {
-        throw new BadRequestError('please provide email and password');
-    }
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw new UnauthenticatedError('Invalid Credentials')
-    }
-    // Compare Password
-    const isPasswordCorrect = await user.comparePassword(password)
-    if (!isPasswordCorrect) {
-        throw new UnauthenticatedError('Invalid Credentials (wrong Password)')
-    }
-    const token = user.createToken();
-    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
-}
+  // console.log(req.headers);
+  const { email, password } = req.body;
+  if ((!email, !password)) {
+    throw new BadRequestError("please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  // Compare Password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials (wrong Password)");
+  }
+  const token = user.createToken();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
 module.exports = {
-    register, login,
-}
+  register,
+  login,
+};
